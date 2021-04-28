2b2b80ced50dadeb2a04153eb9e1723661d78db9
linbo <liyu.linbo@tuya.com>
2021-04-28 14:08:33 +0800

    测试

---
+++ TestCompent/TestCompent/TestCompent.h
@@ -0,0 +1,18 @@
+//
+//  TestCompent.h
+//  TestCompent
+//
+//  Created by 林波 on 2021/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestCompent.
+FOUNDATION_EXPORT double TestCompentVersionNumber;
+
+//! Project version string for TestCompent.
+FOUNDATION_EXPORT const unsigned char TestCompentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCompent/PublicHeader.h>
+
+
---
+++ TestCompent/TestCompentTests/TestCompentTests.m
@@ -0,0 +1,36 @@
+//
+//  TestCompentTests.m
+//  TestCompentTests
+//
+//  Created by 林波 on 2021/4/28.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestCompentTests : XCTestCase
+
+@end
+
+@implementation TestCompentTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
