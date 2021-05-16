e4759caf8d45d428cc1b487dbcf5aa21d5f4999a
linbo <liyu.linbo@tuya.com>
2021-05-16 20:58:02 +0800

    添加YYModel

--- TestCompent/TestCompent/TestModel.m
+++ TestCompent/TestCompent/TestModel.m
@@ -6,12 +6,16 @@
 //
 
 #import "TestModel.h"
+#if ISHOMESDK == 1
+#import <TuyaSmartHomeKit/TuyaSmartKit.h>
+#endif
 
 @implementation TestModel
 
 + (void)testLog{
     NSLog(@"正常打印：%s",__func__);
 #if ISHOMESDK == 1
+    TuyaSmartDevice *device = [[TuyaSmartDevice alloc] initWithDeviceId:@""];
     NSLog(@"HomeSDK打印：%s",__func__);
 #endif
 }
